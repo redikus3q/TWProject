96e482c675158bd7463d8b2ac3a3e94ff473771e
Alexandru-Daniel Tiganus <alexandu24@gmail.com>
2021-05-15 19:28:19 +0300

    v2

--- static/script.js
+++ static/script.js
@@ -1,5 +1,106 @@
 let body = document.getElementsByClassName("content-container")[0];
-body.innerText = "Loading...";
+
+fetch('http://localhost:3000/dogs')
+    .then(
+      function (response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        response.json().then(function (data) {
+          putimg(data);
+        });
+      }
+    )
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
+
+function putimg(dogs){
+  x = document.getElementsByClassName("imgmain")[0];
+  let k=0;
+  for(let i in dogs){
+    if(dogs[i].rating>=4){
+      x.setAttribute("src", dogs[i].img);
+      x.setAttribute("id", "img"+dogs[i].id);
+      document.getElementById("rest-title").innerText = dogs[i].name;
+      document.getElementById("rest-address").innerText = dogs[i].address;
+      document.getElementById("rest-phone").innerText = dogs[i].phone;
+      k=1;
+      break;
+    }
+  }
+  if(k==0){
+    x.setAttribute("src", "https://imgur.com/OaY5R5h.jpg");
+    document.getElementById("rest-title").innerText = "Niciun restaurant";
+  }
+}
+
+function preview(x){
+  fetch('http://localhost:3000/dogs')
+    .then(
+      function (response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        response.json().then(function (data) {
+          solve(data);
+        });
+      }
+    )
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
+  function solve(dogs){
+    let dog = document.getElementsByClassName("imgmain")[0];
+    let y = dog.id[3];
+    let goodDogs=[];
+    for(let i in dogs){
+      if(dogs[i].rating>=4){
+        goodDogs.push(dogs[i]);
+      }
+    }
+    if(goodDogs){
+      let obj = goodDogs[0];
+      let f = 0;
+      if(x=='-'){
+        for(let i in goodDogs){
+          if(goodDogs[i].id < y){
+            obj = goodDogs[i];
+            f=1;
+          }
+        }
+        if(f==0){
+          obj=goodDogs[goodDogs.length-1];
+        }
+      }
+      if(x=='+'){
+        for(let i in goodDogs){
+          if(goodDogs[i].id > y){
+            obj = goodDogs[i];
+            f=1;
+            break;
+          }
+        }
+        if(f==0){
+          obj=goodDogs[0];
+        }
+      }
+      dog.setAttribute("src", obj.img)
+      dog.setAttribute("id", "img"+obj.id);
+      dog.setAttribute("animation", "none");
+      dog.className="imgmain";
+      setTimeout(function(){
+        dog.className="imgmain fade";},10);
+      document.getElementById("rest-title").innerText = obj.name;
+      document.getElementById("rest-address").innerText = obj.address;
+      document.getElementById("rest-phone").innerText = obj.phone;
+    }
+  }
+}
 
 function restaurante(){
   fetch('http://localhost:3000/dogs')
@@ -10,8 +111,6 @@ function restaurante(){
             response.status);
           return;
         }
-
-        // Examine the text in the response
         response.json().then(function (data) {
           console.log(data);
           renderDogs(data);
@@ -23,7 +122,7 @@ function restaurante(){
     });
 
   function renderDogs(dogs) {
-    body.innerText = "";
+    body.innerHTML = "";
     let input1 = document.createElement("input");
     input1.setAttribute("type", "text");
     input1.setAttribute("id", "input1");
@@ -71,18 +170,37 @@ function restaurante(){
 
       img.setAttribute("src", dogs[i].img);
       img.className="rest-img";
-      p = document.createElement('p');
-      p.innerText = dogs[i].name;
-      p.className="rest-name";
-
+      h2 = document.createElement('h2');
+      h2.innerText = dogs[i].name;
+      h2.className="rest-name";
+      ul = document.createElement('ul');
+      ul.className="rest-desc";
+      ul.innerHTML=`
+        <li>
+          <i class="fa fa-star fa-2x"></i>
+          <h1 class="rest-address-2">`+dogs[i].rating+`</h1>
+        </li>
+        <li>
+          <i class="fa fa-map-marker fa-2x"></i>
+          <h1 class="rest-address-2">`+dogs[i].address+`</h1>
+        </li>
+        <li>
+          <i class="fa fa-phone fa-2x"></i>
+          <h1 class="rest-address-2">`+dogs[i].phone+`</h1>
+        </li>
+      `
+      body.setAttribute("style", "background-color:#ddd;");
       buttonss = document.createElement('div');
       buttonss.className="rest-button-container";
-      div.appendChild(p);
-      div.appendChild(img);
+      newdiv=document.createElement("div");
+      newdiv.setAttribute("class", "rest-img-div");
+      newdiv.appendChild(img);
+      div.appendChild(newdiv);
+      div.appendChild(h2);
+      div.appendChild(ul);
       buttonss.appendChild(edit);
       buttonss.appendChild(del)
       div.appendChild(buttonss);
-      p.setAttribute("style", "font-size:40px;");
       div.className="rest-container";
       body.appendChild(div);
       input = document.createElement("input");
